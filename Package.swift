91055c8a53829b5d5237ac51e4840f5b68bf3b66
Tim Morgan <github@timothymorgan.info>
2024-04-04 00:09:14 -0700

    Lock down version requirements and version test coverage

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftAirmen",
     defaultLocalization: "en",
-    platforms: [.macOS(.v12), .iOS(.v15), .tvOS(.v14), .watchOS(.v8)],
+    platforms: [.macOS(.v13), .iOS(.v16), .watchOS(.v9), .tvOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
