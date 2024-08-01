b99c6a17bdabdb38b2ea408a8a362f6570c0886f
Tim Morgan <github@timothymorgan.info>
2024-07-31 17:49:14 -0700

    Package updates

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/RISCfuture/csv.swift.git", branch: "master"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
         .package(url: "https://github.com/marmelroy/Zip.git", from: "2.1.0"),
-        .package(url: "https://github.com/jkandzi/Progress.swift", from: "0.4.0")
+        .package(url: "https://github.com/jkandzi/Progress.swift.git", from: "0.4.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
